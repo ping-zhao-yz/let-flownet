[voxel] fix the eventmap channel issue; remove the dead head code to save computation resource; increase worker number for data loading

main_voxel_snntrans_grayscale_dt1.py
@@ -104,9 +104,12 @@ def train(train_loader, model, optimizer, epoch, train_writer):
             flow_predictions = model(event_data, image_resize, sp_threshold)
 
             # Photometric loss (sum along dim=4 to get the dense spatial map mask)
+            # Sum over both channels (dim=1) and time (dim=4) to get a flat [Batch, H, W] spatial mask
+            event_mask = torch.sum(event_data, dim=(1, 4)) 
+
             photometric_loss = photometric_loss_backward_single(
                 former_gray[:, 0, :, :].to(device), latter_gray[:, 0, :, :].to(device), 
-                torch.sum(event_data, dim=4), flow_predictions, device, print_details, weights=multiscale_weights)
+                event_mask, flow_predictions, device, print_details, weights=multiscale_weights)
 
             # Smoothness loss
             smoothness_loss = smooth_loss_single(flow_predictions)
@@ -287,7 +290,7 @@ def validate(test_loader, model, epoch, output_writers):
 def main():
     global args
 
-    workers = 4
+    workers = 8
     best_EPE = -1
     evaluate_interval = 3
 

models/let_flownet_evtcount.py
@@ -50,7 +50,6 @@ class Let_Flownet_EvtCount(BaseModel):
 
         # Transformers
         norm = self.args.norm
-        self.head = ConvLayer(in_channels=4, out_channels=64, kernel_size=5, stride=1, padding=2, norm=norm)
 
         self.position_embedding = build_position_encoding('sine', 512)
         self.split0 = nn.Unfold(kernel_size=1, stride=1, padding=0)
@@ -171,10 +170,8 @@ class Let_Flownet_EvtCount(BaseModel):
         """
 
         #************* path to transformer
-        # TODO: 5 - verify the impact of using the event at the first time slot
-        head = self.head(input[:, :, :, :, 0])
-        
-        n, c, H, W = head.size()
+        # Extract spatial dimensions directly from the SNN features
+        n, c, H, W = blocks[-4].size()
 
         # Small -> Big
         #******** scale 0