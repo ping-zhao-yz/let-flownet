ba71d0899f210c1f0f424df595d726cb3558db22
Ping Zhao <ping.zhao.yz@gmail.com>
2026-05-24 12:58:22 +1000

    [voxel] troubleshoot the massive training skipping issue - fix the missing polarity issue

--- datasets/voxel/voxel_grid.py
+++ datasets/voxel/voxel_grid.py
@@ -15,7 +15,7 @@ def events_to_voxel_grid(events, num_bins, height, width, device=device):
         events_torch = torch.from_numpy(events).to(device)
 
         # Initialize the voxel grid on GPU
-        voxel_grid = torch.zeros(num_bins, height, width, dtype=torch.float32, device=device).flatten()
+        voxel_grid = torch.zeros(num_bins, height, width, 2, dtype=torch.float32, device=device).flatten(end_dim=2)
 
         # normalize the event timestamps so that they lie between 0 and num_bins
         last_stamp = events_torch[-1, 0]
@@ -33,27 +33,43 @@ def events_to_voxel_grid(events, num_bins, height, width, device=device):
         pols = events_torch[:, 3].float()
         pols[pols == 0] = -1  # polarity should be +1 / -1
 
+        mask_pos = (pols == 1)
+        mask_neg = (pols == -1)
+
         tis = torch.floor(ts)
         tis_long = tis.long()
         dts = ts - tis
-        vals_left = pols * (1.0 - dts.float())
-        vals_right = pols * dts.float()
+        vals_left = 1.0 - dts.float()
+        vals_right = dts.float()
 
         valid_indices = tis < num_bins
         valid_indices &= tis >= 0
-        voxel_grid.index_add_(dim=0,
-                                index=xs[valid_indices] + ys[valid_indices]
-                                * width + tis_long[valid_indices] * width * height,
-                                source=vals_left[valid_indices])
+
+        datatype = torch.cuda.LongTensor
+
+        voxel_grid[:,0].index_add_(dim=0,
+                                index=(xs[valid_indices*mask_pos] + ys[valid_indices*mask_pos]
+                                        * width + tis_long[valid_indices*mask_pos] * width * height).type(
+                                    datatype),
+                                source=vals_left[valid_indices*mask_pos])
+        voxel_grid[:,1].index_add_(dim=0,
+                                    index=(xs[valid_indices * mask_neg] + ys[valid_indices * mask_neg]
+                                            * width + tis_long[valid_indices * mask_neg] * width * height).type(
+                                        datatype),
+                                    source=vals_left[valid_indices*mask_neg])
 
         valid_indices = (tis + 1) < num_bins
         valid_indices &= tis >= 0
 
-        voxel_grid.index_add_(dim=0,
-                                index=xs[valid_indices] + ys[valid_indices] * width
-                                + (tis_long[valid_indices] + 1) * width * height,
-                                source=vals_right[valid_indices])
+        voxel_grid[:,0].index_add_(dim=0,
+                                index=(xs[valid_indices*mask_pos] + ys[valid_indices*mask_pos] * width
+                                        + (tis_long[valid_indices*mask_pos] + 1) * width * height).type(datatype),
+                                source=vals_right[valid_indices*mask_pos])
+        voxel_grid[:,1].index_add_(dim=0,
+                                index=(xs[valid_indices*mask_neg] + ys[valid_indices*mask_neg] * width
+                                        + (tis_long[valid_indices*mask_neg] + 1) * width * height).type(datatype),
+                                source=vals_right[valid_indices*mask_neg])
 
-        voxel_grid = voxel_grid.view(num_bins, height, width)
+        voxel_grid = voxel_grid.view(num_bins, height, width, 2).permute(0, 3, 1, 2)
 
     return voxel_grid
