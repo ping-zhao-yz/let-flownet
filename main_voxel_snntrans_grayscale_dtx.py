737431793ea527762f18fe7b80a6d20a39dd5d9a
Ping Zhao <ping.zhao.yz@gmail.com>
2026-09-01 22:48:38 +1000

    [h5] optimize the logs

--- main_voxel_snntrans_grayscale_dtx.py
+++ main_voxel_snntrans_grayscale_dtx.py
@@ -82,10 +82,7 @@ parser.add_argument('--train_host', default='local', choices=['local', 'h200'],
                     help='Host environment to determine dataset paths')
 
 args = parser.parse_args()
-
-# Initializations
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(f"=> using device '{device}'")
 
 image_resize = 256
 sp_threshold = args.sp_threshold
@@ -395,6 +392,8 @@ def validate(test_loader, model, epoch, output_writers, current_test_src_file, c
 
 def main():
     global args
+    # Initializations
+    print(f"=> using device '{device}'")
 
     workers = 8
     best_EPE = -1
